97ed65a18e38f7543725b2fa0a05e930f2ad8221
Stephane Prevert <prevert@winch.fr>
2021-04-07 10:17:16 +0200

    Updated titres

--- pages/titres.js
+++ pages/titres.js
@@ -1,11 +1,15 @@
-import Image from 'next/image'
-import Layout from '../components/layout'
-import { Card, CardGroup } from 'react-bootstrap'
+﻿import Image from 'next/image'
+import Layout from '../components/Layout'
+import Livres from '../components/Livres';
+
 export default function Home() {
 	return (
 		<Layout>
 			<h1 className="titres"><Image src="/img/logo/Picto-titres-blanc.svg" alt="Mes titres" height={50} width={50} />
-			Mes titres</h1>			
+			Mes titres</h1>
+			<div class="cTable">
+				<Livres />
+			</div>
 		</Layout>
 	)
 }
