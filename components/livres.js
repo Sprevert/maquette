8189c6d19d3976c9b52381f88870db191bd1d114
Stephane Prevert <prevert@winch.fr>
2021-05-04 10:37:15 +0200

    Updated mes livres

--- components/livres.js
+++ components/livres.js
@@ -12,11 +12,11 @@ const columns = [
 ];
 
 const rows = [
-	{ id: 4, ean: '9782080238047', titre: 'La Princesse au petit moi', maison: 'FLAMMARION', parution: '07/04/2021', format: 'LIVRE', ventes2020: 5000, ventes: 100000 },
-	{ id: 2, ean: '9782081420250', titre: 'Les trois femmes du consul', maison: 'FLAMMARION', parution: '09/10/2019', format: 'LIVRE', ventes2020: 5000, ventes: 100000 },
-	{ id: 0, ean: '9782070146413', titre: 'Check - point', maison: 'BLANCHE', parution: '10/04/2015', format: 'LIVRE', ventes2020: 1000, ventes: 200000 },
-	{ id: 1, ean: '9782070455379', titre: 'Immortelle randonnée - Compostelle malgré moi', maison: 'FOLIO', parution: '02/10/2014', format: 'LIVRE', ventes2020: 8000, ventes: 180000 },	
-	{ id: 3, ean: '9782290005569', titre: 'Le parfum d\'Adam', maison: 'J\'AI LU', parution: '10/01/2007', format: 'LIVRE', ventes2020: 500, ventes: 10000 },
+	{ id: 4, ean: '9782080238047', titre: 'La Princesse au petit moi',soustitre:'', maison: 'FLAMMARION', parution: '07/04/2021', format: 'LIVRE', ventes2020: 5000, ventes: 100000 },
+	{ id: 2, ean: '9782081420250', titre: 'Les trois femmes du consul', soustitre: '', maison: 'FLAMMARION', parution: '09/10/2019', format: 'LIVRE', ventes2020: 5000, ventes: 100000 },
+{ id: 0, ean: '9782070146413', titre: 'Check - point', soustitre: '', maison: 'BLANCHE', parution: '10/04/2015', format: 'LIVRE', ventes2020: 1000, ventes: 200000 },
+	{ id: 1, ean: '9782070455379', titre: 'Immortelle randonnée', soustitre: 'Compostelle malgré moi', maison: 'FOLIO', parution: '02/10/2014', format: 'LIVRE', ventes2020: 8000, ventes: 180000 },	
+{ id: 3, ean: '9782290005569', titre: 'Le parfum d\'Adam', soustitre: '', maison: 'J\'AI LU', parution: '10/01/2007', format: 'LIVRE', ventes2020: 500, ventes: 10000 },
 ];
 
 function numberWithSpaces(x) {
@@ -46,11 +46,12 @@ export default function DataTable() {
 									src={require("assets/img/couverture/" + row.ean + ".jpg")}
 								/></td>
 								<td>
-									<div className="livresh1">{row.titre}</div>
+									<div className={row.soustitre == '' ? "livresh1" : "livresh1 mb-0"}>{row.titre}</div>
+									{row.soustitre != '' && <div className="livresh2">{row.soustitre}</div>}
 									<div className="mobileTable">{row.maison} - {row.parution}</div>
 									<div className="mobileTable">Sortie caisse :</div>
-									<div className="mobileTable text-right">{numberWithSpaces(row.ventes2020)} (semaine)</div>
-									<div className="mobileTable text-right">{numberWithSpaces(row.ventes)} (depuis parution)</div></td>
+									<div className="mobileTable ml-5">{numberWithSpaces(row.ventes2020)} (semaine)</div>
+									<div className="mobileTable ml-5">{numberWithSpaces(row.ventes)} (depuis parution)</div></td>
 								
 								<td className="fullTable text-center">{row.maison}</td>
 								<td className="fullTable text-center">{row.parution}</td>
